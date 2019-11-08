c836c1fba82e51b06e6acde93d5aec9697af873b
didin jamaludin <didinj@gmail.com>
2019-11-08 18:30:26 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ionic 4 Tutorial: Ionic Responsive Grid Angular 8 Examples
+
+This source code is part of [Ionic 4 Tutorial: Ionic Responsive Grid Angular 8 Examples]().
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
+  articles: any[] = [
+    { title: 'Grails 4 Tutorial: Spring Security Core Login Example', image: './assets/imgs/article1.png' },
+    { title: 'Angular Material Form Controls, Form Field and Input Examples', image: './assets/imgs/article2.png' },
+    { title: 'Angular 8 Tutorial: Observable and RXJS Examples', image: './assets/imgs/article3.png' },
+    { title: 'React Native Tutorial: Facebook Login Example', image: './assets/imgs/article4.png' },
+    { title: 'Spring Boot Tutorial: Build an MVC Java Web App using Netbeans', image: './assets/imgs/article5.png' },
+    { title: 'Ionic 4 Tutorial: Facebook Login Example', image: './assets/imgs/article6.png' },
+    { title: 'View Google Maps in HTML Page', image: './assets/imgs/article7.png' },
+    { title: 'Angular 8 Tutorial: REST API and HttpClient Examples', image: './assets/imgs/article8.png' },
+    { title: 'MEAN Stack (Angular 8) Tutorial: Build a Simple Blog CMS', image: './assets/imgs/article9.png' },
+    { title: 'MongoDB Tutorial: Aggregate Method Example', image: './assets/imgs/article10.png' },
+    { title: 'Ionic 4 Tutorial: Angular 8 Multilevel Accordion Menu Example', image: './assets/imgs/article11.png' },
+    { title: 'React Native Firebase Cloud Messaging (FCM) Push Notification', image: './assets/imgs/article12.png' }
+  ];
+
   constructor() {}
 
 }
